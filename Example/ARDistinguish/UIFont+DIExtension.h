654f0ada2331024dbb2022bef64ce31d85c289e4
KuaShen <shengjf154062261@qq.com>
2019-04-30 15:25:29 +0800

    AR识别第一次提交git

---
+++ Example/ARDistinguish/UIFont+DIExtension.h
@@ -0,0 +1,17 @@
+//
+//  UIFont+DIExtension.h
+//  ARDistinguish_Example
+//
+//  Created by mac on 2019/4/29.
+//  Copyright © 2019 KuaShen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIFont (DIExtension)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/ARDistinguish/UIFont+DIExtension.m
@@ -0,0 +1,13 @@
+//
+//  UIFont+DIExtension.m
+//  ARDistinguish_Example
+//
+//  Created by mac on 2019/4/29.
+//  Copyright © 2019 KuaShen. All rights reserved.
+//
+
+#import "UIFont+DIExtension.h"
+
+@implementation UIFont (DIExtension)
+
+@end
